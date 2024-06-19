173de432e6efb2f801bc5ebf208d2efec4e72916
HanabiKudo <retro.mendozaa@gmail.com>
2024-06-19 15:03:05 -0600

    added pages

---
+++ src/pages/message/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import FloatingNav from "@/components/sideBar";
+import Sidebar from "@/components/sideBar";
+import Topbar from "@/components/topBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+     <Sidebar></Sidebar>
+     <Topbar></Topbar>
+    </>
+  );
+}

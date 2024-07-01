b575c5d95363bdeb3bc8ff6ab1db8ddbe6fe4541
Jaredgg4 <jg70456898@gmail.com>
2024-07-01 12:28:27 -0600

    created a user model with all of its properties

---
+++ src/models/user.ts
@@ -0,0 +1,13 @@
+export type APPuser = {
+    uID: string,
+    fullName: string,
+    uEmail: string,
+    uPassword: string,
+    ppURL: string,
+    timeSlots: string[],
+    DOB: string,
+    phone: number,
+    address: string,
+    zip: number,
+    country: string
+}
